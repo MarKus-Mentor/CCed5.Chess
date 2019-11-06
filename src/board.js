dede6ef688d4b5c23f222d9f73d3cd6353decac5
Mateusz <kontakt.zajac@gmail.com>
2019-11-06 22:25:20 +0100

    Add knights to board

--- src/board.js
+++ src/board.js
@@ -1,4 +1,5 @@
 import Pawn from './pieces/pawn';
+import Knight from './pieces/knight';
 
 const board = new Array(8);
 for (let i = 0; i < 8; i++) {
@@ -9,5 +10,14 @@ let pawn = new Pawn(6, 0, 'white');
 board[pawn.x][pawn.y] = pawn;
 pawn = new Pawn(6, 1, 'white');
 board[pawn.x][pawn.y] = pawn;
+
+let knight = new Knight(7, 1, 'white');
+board[knight.x][knight.y] = knight;
+knight = new Knight(7, 6, 'white');
+board[knight.x][knight.y] = knight;
+knight = new Knight(0, 1, 'black');
+board[knight.x][knight.y] = knight;
+knight = new Knight(0, 6, 'black');
+board[knight.x][knight.y] = knight;
 
 export default board;
