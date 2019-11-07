c0a514d78bed07e2bcbc428b5938ee0ad44424bd
kontaktzajac <kontakt.zajac@gmail.com>
2019-11-07 12:29:27 +0100

    Separation functions of field selection for individual figures

--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -6,14 +6,14 @@ class Knight extends Piece {
     this.name = 'knight';
     this.display = `<i class="fas fa-chess-knight ${side}"></i>`; //fontawesome knight
   }
-  findLegalMoves() {
+  findLegalMovesKnight() {
     const possibleMoves = [];
-    //Pierwsza możliwość Dwie kratki po Y, jedna po X
+    //First possibility: two grids on Y, one grid on X
       this.x - 2 >= 0 && this.y - 1 >= 0 && possibleMoves.push(`${this.x - 2},${this.y-1}`);
       this.x - 2 >= 0 && this.y + 1 < 8 && possibleMoves.push(`${this.x - 2},${this.y+1}`);
       this.x + 2 < 8 && this.y - 1 >= 0 && possibleMoves.push(`${this.x + 2},${this.y-1}`);
       this.x + 2 < 8 && this.y + 1 < 8 && possibleMoves.push(`${this.x + 2},${this.y+1}`);
-    //Druga możliwość Dwie kratki po X, jedna po Y      
+    //Second possibility: one grid on Y, two grids on X   
       this.x - 1 >= 0 && this.y - 2 >= 0 && possibleMoves.push(`${this.x - 1},${this.y-2}`);
       this.x - 1 >= 0 && this.y + 2 < 8 && possibleMoves.push(`${this.x - 1},${this.y+2}`);
       this.x + 1 < 8 && this.y - 2 >= 0 && possibleMoves.push(`${this.x + 1},${this.y-2}`);
--- src/touched.js
+++ src/touched.js
@@ -6,7 +6,18 @@ const touched = e => {
   if (!board[x][y]) {
     return;
   }
-  const possibleMoves = board[x][y].findLegalMoves();
+
+  let possibleMoves = board[x][y];
+  switch (board[x][y].name)
+  {
+    case 'pawn': possibleMoves = board[x][y].findLegalMovesPawn(); break;
+    case 'rook': possibleMoves = board[x][y].findLegalMovesRook(); break;
+    case 'knight': possibleMoves = board[x][y].findLegalMovesKnight(); break;
+    case 'bishop': possibleMoves = board[x][y].findLegalMovesBishop(); break;
+    case 'king': possibleMoves = board[x][y].findLegalMovesKing(); break;
+    case 'queen': possibleMoves = board[x][y].findLegalMovesQueen(); break;
+  }
+
   for (let el of possibleMoves) {
     document.getElementById(el).className += ` possibleMove`;
     document.getElementById(el).addEventListener('click', e => {
