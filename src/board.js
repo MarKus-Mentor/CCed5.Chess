d9fc247098779a627a4e3295e265fb42582cb09e
luizamich <57197259+luizamich@users.noreply.github.com>
2019-11-08 00:07:00 +0100

    placing rooks on the board

--- src/board.js
+++ src/board.js
@@ -10,4 +10,13 @@ board[pawn.x][pawn.y] = pawn;
 pawn = new Pawn(6, 1, 'white');
 board[pawn.x][pawn.y] = pawn;
 
+let rook = new Rook(7,0,'white');
+board[rook.x][rook.y] = rook;
+rook = new Rook(7,7,'white');
+board[rook.x][rook.y] = rook;
+rook = new Rook(0,0,'black');
+board[rook.x][rook.y] = rook;
+rook = new Rook(0,7,'black');
+board[rook.x][rook.y] = rook;
+
 export default board;
