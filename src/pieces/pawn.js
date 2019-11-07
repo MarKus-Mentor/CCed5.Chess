ae5c3377f7cf5b5706c3264eae587b247ea107e8
JoannaDS <j.dabrowska.slaby@gmail.com>
2019-11-07 14:42:00 +0100

    Trying to configure the moves

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import { WSA_E_CANCELLED } from 'constants';
 
 class Pawn extends Piece {
   constructor(x, y, side) {
@@ -6,15 +7,29 @@ class Pawn extends Piece {
     this.name = 'pawn';
     this.display = `<i class="fas fa-chess-pawn ${side}"></i>`;
   }
+
+  // clickHandler() {
+  // this.x.pawnWasClicked = true;
+  // }
+
+  // pawn.addEventListener('click', clickHandler);
+
   findLegalMoves() {
     // console.log(this.x, this.y);
+    let pawnWasClicked = false;
+
     const possibleMoves = [];
-    if (this.side == 'white') {
+
+    if (this.side == 'white' && pawnWasClicked) {
       this.x - 1 > 0 && possibleMoves.push(`${this.x - 1},${this.y}`);
       this.x - 2 > 0 && possibleMoves.push(`${this.x - 2},${this.y}`);
+    } else {
+      this.x + 1 > 0 && possibleMoves.push(`${this.x + 1},${this.y}`);
+      this.x + 2 > 0 && possibleMoves.push(`${this.x + 2},${this.y}`);
     }
     return possibleMoves;
   }
+
   promote() {}
   enPassant() {}
 }
