793441964b62221c7df8dcc560d0118572630ead
Joanna DS <j.dabrowska.slaby@gmail.com>
2019-11-08 21:25:58 +0100

    Adding more pawn moves

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -21,39 +21,41 @@ class Pawn extends Piece {
         console.log(!!board[2][0]);
         console.log(!!board[1][0]);
       }
-      if (
-        board[this.x - 1][this.y - 1] &&
-        board[this.x - 1][this.y - 1].side === "black"
-      ) {
-        this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y - 1}`);
-      }
-      if (
-        board[this.x - 1][this.y + 1] &&
-        board[this.x - 1][this.y + 1].side === "black"
-      ) {
-        this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y + 1}`);
-      }
     }
-
+    if (
+      this.side === "white" &&
+      board[this.x - 1][this.y - 1] &&
+      board[this.x - 1][this.y - 1].side === "black"
+    ) {
+      this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y - 1}`);
+    }
+    if (
+      this.side === "white" &&
+      board[this.x - 1][this.y + 1] &&
+      board[this.x - 1][this.y + 1].side === "black"
+    ) {
+      this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y + 1}`);
+    }
     if (this.side === "black" && !board[this.x + 1][this.y]) {
       this.x + 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y}`);
       if (this.x === 1 && !board[this.x + 2][this.y]) {
         this.x + 2 <= 7 && possibleMoves.push(`${this.x + 2},${this.y}`);
       }
-      if (
-        board[this.x + 1][this.y - 1] &&
-        board[this.x + 1][this.y + 1].side === "white"
-      ) {
-        this.x - 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y - 1}`);
-      }
-      if (
-        board[this.x + 1][this.y + 1] &&
-        board[this.x + 1][this.y + 1].side === "white"
-      ) {
-        this.x - 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y + 1}`);
-      }
     }
-
+    if (
+      this.side === "black" &&
+      board[this.x + 1][this.y - 1] &&
+      board[this.x + 1][this.y - 1].side === "white"
+    ) {
+      this.x - 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y - 1}`);
+    }
+    if (
+      this.side === "black" &&
+      board[this.x + 1][this.y + 1] &&
+      board[this.x + 1][this.y + 1].side === "white"
+    ) {
+      this.x - 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y + 1}`);
+    }
     if (board[this.x - 1][this.y]) {
       console.log("dziala");
     }
