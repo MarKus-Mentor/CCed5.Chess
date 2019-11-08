ae1cd11aea06eadbe79380a50b820cb981a0a213
Adrian <adrian.halawczak@gmail.com>
2019-11-08 23:55:54 +0100

    Ustawienie na planszy i zadeklarowanie ruchow

--- src/board.js
+++ src/board.js
@@ -1,4 +1,5 @@
 import Pawn from './pieces/pawn';
+import Queen from './pieces/queen';
 
 const board = new Array(8);
 for (let i = 0; i < 8; i++) {
@@ -9,5 +10,11 @@ let pawn = new Pawn(6, 0, 'white');
 board[pawn.x][pawn.y] = pawn;
 pawn = new Pawn(6, 1, 'white');
 board[pawn.x][pawn.y] = pawn;
+
+let queen = new Queen(7, 3, 'white');
+board[queen.x][queen.y] = queen;
+queen = new Queen(0, 3, 'black');
+board[queen.x][queen.y] = queen;
+
 
 export default board;
--- src/pieces/queen.js
+++ src/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import board from '../board';
 
 class Queen extends Piece {
   constructor(x, y, side) {
@@ -9,6 +10,25 @@ class Queen extends Piece {
   findLegalMoves() {
     const possibleMoves = [];
 
+    for(let i=1; i<8; i++) {
+      // ruchy w gore
+      this.x - i >= 0 && possibleMoves.push(`${this.x - i},${this.y}`);
+      // ruchy skosne prawo && gora
+      this.x - i >= 0 && this.y + i <= 7 && possibleMoves.push(`${this.x - i},${this.y + i}`);
+      // ruchy w prawo
+      this.y + i <= 7 && possibleMoves.push(`${this.x},${this.y + i}`);
+      // ruchy skosne prawo && dol
+      this.x + i <= 7 && this.y + i <= 7 && possibleMoves.push(`${this.x + i},${this.y + i}`);
+      // ruchy w dol
+      this.x + i <= 7 && possibleMoves.push(`${this.x + i},${this.y}`);
+      // ruchy skosne lewo && dol
+      this.x + i <= 7 && this.y - i >= 0 && possibleMoves.push(`${this.x + i},${this.y - i}`);
+      // ruchy w lewo
+      this.y - i >= 0 && possibleMoves.push(`${this.x},${this.y - i}`);
+      // ruchy skosne lewo && gora
+      this.x - i >= 0 && this.y - i >= 0 && possibleMoves.push(`${this.x - i},${this.y - i}`);
+    }
+    
     return possibleMoves;
   }
 }
