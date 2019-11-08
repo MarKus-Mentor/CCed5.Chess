6a5bd744c4d5dfd3f01fa99afcbbc6e37efa3d0b
Ola <aleksandra@gruchala.eu>
2019-11-08 20:02:37 +0100

    Update the board in local storage after each move

--- src/board.js
+++ src/board.js
@@ -1,4 +1,9 @@
 import Pawn from './pieces/pawn';
+import King from "./pieces/king";
+import Queen from "./pieces/queen";
+import Rook from "./pieces/rook";
+import Knight from "./pieces/knight";
+import Bishop from "./pieces/bishop";
 
 const board = new Array(8);
 for (let i = 0; i < 8; i++) {
@@ -11,7 +16,6 @@ let boardStorage = JSON.parse(localStorage.getItem("board"));
 console.log(boardStorage);
 
 if (boardStorage) {
-
   for (let x = 0; x < boardStorage.length; x++) {
     for (let y = 0; y < boardStorage.length; y++) {
       let piece = boardStorage[x][y];
@@ -22,8 +26,26 @@ if (boardStorage) {
             instance = new Pawn(piece.x, piece.y, piece.side);
             board[instance.x][instance.y] = instance;
             break;
-
-            //todo: kolejne case'y dla wszystkich figur
+          case "king":
+            instance = new King(piece.x, piece.y, piece.side);
+            board[instance.x][instance.y] = instance;
+            break;
+          case "queen":
+            instance = new Queen(piece.x, piece.y, piece.side);
+            board[instance.x][instance.y] = instance;
+            break;
+          case "rook":
+            instance = new Rook(piece.x, piece.y, piece.side);
+            board[instance.x][instance.y] = instance;
+            break;
+          case "knight":
+            instance = new Knight(piece.x, piece.y, piece.side);
+            board[instance.x][instance.y] = instance;
+            break;
+          case "bishop":
+            instance = new Bishop(piece.x, piece.y, piece.side);
+            board[instance.x][instance.y] = instance;
+            break;
         }
         boardStorage[x][y] = instance;
       }
--- src/pieces/piece.js
+++ src/pieces/piece.js
@@ -12,6 +12,7 @@ class Piece {
     const newY = Number(id[2]);
 
     //clearing previous place
+    const oldXY = {...board[this.x][this.y]};
     board[this.x][this.y] = null;
     document.getElementById(`${this.x},${this.y}`).innerHTML = '';
 
@@ -19,11 +20,23 @@ class Piece {
     this.x = newX;
     this.y = newY;
     board[this.x][this.y] = this;
+    const newXY = {...board[this.x][this.y]};
     document.getElementById(id).innerHTML = this.display;
 
     // log move to the console
     const yArr = ["A", "B", "C", "D", "E", "F", "G", "H"];
-    console.log(`${this.side} ${this.name} go to ${yArr[this.y]}-${this.x}`);
+    const figure = `${this.side} ${this.name}`;
+    const figureLocation = `${yArr[this.y]}${this.x}`;
+    console.log(`${figure} go to ${figureLocation}`);
+
+    // add move to localStorage
+    const boardStorage = JSON.parse(localStorage.getItem("board"));
+    let piecePosition = boardStorage[oldXY.x][oldXY.y];
+    piecePosition.x = newXY.x;
+    piecePosition.y = newXY.y;
+    boardStorage[newXY.x][newXY.y] = piecePosition;
+    boardStorage[oldXY.x][oldXY.y] = null;
+    localStorage.setItem("board", JSON.stringify(boardStorage));
   }
 
   findLegalMoves() {}
