f0c3d915acf74e771299ffadfc57f078a66c6c07
Adrian <adrian.halawczak@gmail.com>
2019-11-09 17:27:37 +0100

    Updated function findLegalMoves()

--- src/pieces/queen.js
+++ src/pieces/queen.js
@@ -7,26 +7,100 @@ class Queen extends Piece {
     this.name = 'queen';
     this.display = `<i class="fas fa-chess-queen ${side}"></i>`; //fontawesome queen
   }
+  
   findLegalMoves() {
     const possibleMoves = [];
 
+    // ruchy w gore
     for(let i=1; i<8; i++) {
-      // ruchy w gore
-      this.x - i >= 0 && possibleMoves.push(`${this.x - i},${this.y}`);
-      // ruchy skosne prawo && gora
-      this.x - i >= 0 && this.y + i <= 7 && possibleMoves.push(`${this.x - i},${this.y + i}`);
-      // ruchy w prawo
-      this.y + i <= 7 && possibleMoves.push(`${this.x},${this.y + i}`);
-      // ruchy skosne prawo && dol
-      this.x + i <= 7 && this.y + i <= 7 && possibleMoves.push(`${this.x + i},${this.y + i}`);
-      // ruchy w dol
-      this.x + i <= 7 && possibleMoves.push(`${this.x + i},${this.y}`);
-      // ruchy skosne lewo && dol
-      this.x + i <= 7 && this.y - i >= 0 && possibleMoves.push(`${this.x + i},${this.y - i}`);
-      // ruchy w lewo
-      this.y - i >= 0 && possibleMoves.push(`${this.x},${this.y - i}`);
-      // ruchy skosne lewo && gora
-      this.x - i >= 0 && this.y - i >= 0 && possibleMoves.push(`${this.x - i},${this.y - i}`);
+      // sprawdzamy czy jest takie pole
+      if(this.x - i >= 0) {
+        // jesli puste to mozna i sprawdzamy kolejne
+        if (!board[this.x - i][this.y])
+        possibleMoves.push(`${this.x - i},${this.y}`);
+        // jesli zajete przez przeciwny kolor to mozna, ale dalej juz nie
+        else if (this.side !== board[this.x - i][this.y].side)
+        { possibleMoves.push(`${this.x - i},${this.y}`); break; }
+        // jesli zajete przez ten sam kolor to nie mozna i dalej tez nie
+        else break;
+      }
+    }
+
+    // ruchy w skosne prawo && gora
+    for(let i=1; i<8; i++) {
+      if(this.x - i >= 0 && this.y + i <= 7) {
+        if(!board[this.x - i][this.y + i])
+        possibleMoves.push(`${this.x - i},${this.y + i}`);
+        else if (this.side !== board[this.x - i][this.y + i].side)
+        { possibleMoves.push(`${this.x - i},${this.y + i}`); break; }
+        else break;
+      }
+    }
+
+    // ruchy w prawo
+    for(let i=1; i<8; i++) {
+      if(this.y + i <= 7) {
+        if(!board[this.x][this.y + i])
+        possibleMoves.push(`${this.x},${this.y + i}`);
+        else if (this.side !== board[this.x][this.y + i].side)
+        { possibleMoves.push(`${this.x},${this.y + i}`); break; }
+        else break;
+      }
+    }
+
+    // ruchy w skosne prawo && dol
+    for(let i=1; i<8; i++) {  
+      if(this.x + i <= 7 && this.y + i <= 7) {
+        if(!board[this.x + i][this.y + i])
+        possibleMoves.push(`${this.x + i},${this.y + i}`);
+        else if (this.side !== board[this.x + i][this.y + i].side)
+        { possibleMoves.push(`${this.x + i},${this.y + i}`); break; }
+        else break;
+      }
+    }
+
+    // ruchy w dol
+    for(let i=1; i<8; i++) {
+      if(this.x + i <= 7) {
+        if(!board[this.x + i][this.y])
+        possibleMoves.push(`${this.x + i},${this.y}`);
+        else if (this.side !== board[this.x + i][this.y].side)
+        { possibleMoves.push(`${this.x + i},${this.y}`); break; }
+        else break;
+      }  
+    }
+
+    // ruchy w skosne lewo && dol
+    for(let i=1; i<8; i++) {
+      if(this.x + i <= 7 && this.y - i >= 0) {
+        if(!board[this.x + i][this.y - i])
+        possibleMoves.push(`${this.x + i},${this.y - i}`);
+        else if (this.side !== board[this.x + i][this.y - i].side)
+        { possibleMoves.push(`${this.x + i},${this.y - i}`); break; }
+        else break;
+      }
+    }
+
+    // ruchy w lewo
+    for(let i=1; i<8; i++) {
+      if(this.y - i >= 0) {
+        if(!board[this.x][this.y - i])
+        possibleMoves.push(`${this.x},${this.y - i}`);
+        else if (this.side !== board[this.x][this.y - i].side)
+        { possibleMoves.push(`${this.x},${this.y - i}`); break; }
+        else break;
+      }
+    }
+
+    // ruchy w skosne lewo && gora
+    for(let i=1; i<8; i++) {
+      if(this.x - i >= 0 && this.y - i >= 0) {
+        if(!board[this.x - i][this.y - i])
+        possibleMoves.push(`${this.x - i},${this.y - i}`);
+        else if (this.side !== board[this.x - i][this.y - i].side)
+        { possibleMoves.push(`${this.x - i},${this.y - i}`); break; }
+        else break;
+      }
     }
     
     return possibleMoves;
