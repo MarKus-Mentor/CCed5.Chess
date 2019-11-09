87fc4a06f77bfb4e22e834a3df187cd1552d8b43
Joanna DS <j.dabrowska.slaby@gmail.com>
2019-11-09 23:38:57 +0100

    Pawn promotion

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -9,56 +9,59 @@ class Pawn extends Piece {
   }
   findLegalMoves() {
     const possibleMoves = [];
-    if (this.side === "white" && this.x !== 0) {
-      if (!board[this.x - 1][this.y]) {
-        this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y}`);
+
+    if (this.side === "white") {
+      if (this.x !== 0) {
+        if (!board[this.x - 1][this.y]) {
+          this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y}`);
+        }
+        if (this.x === 6 && !board[this.x - 2][this.y]) {
+          this.x - 2 >= 0 && possibleMoves.push(`${this.x - 2},${this.y}`);
+        }
+        if (
+          board[this.x - 1][this.y - 1] &&
+          board[this.x - 1][this.y - 1].side === "black"
+        ) {
+          this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y - 1}`);
+        }
+        if (
+          board[this.x - 1][this.y + 1] &&
+          board[this.x - 1][this.y + 1].side === "black"
+        ) {
+          this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y + 1}`);
+        }
       }
-      if (this.x === 6 && !board[this.x - 2][this.y]) {
-        this.x - 2 >= 0 && possibleMoves.push(`${this.x - 2},${this.y}`);
-      }
-      if (
-        board[this.x - 1][this.y - 1] &&
-        board[this.x - 1][this.y - 1].side === "black"
-      ) {
-        this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y - 1}`);
-      }
-      if (
-        board[this.x - 1][this.y + 1] &&
-        board[this.x - 1][this.y + 1].side === "black"
-      ) {
-        this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y + 1}`);
-      }
-    }
-    if (this.side === "white" && this.x === 0) {
-      alert("wybierz bialego pionka");
-      console.table(board);
-      console.log(this.x, this.y);
-    }
-    if (this.side === "black" && this.x !== 7) {
-      if (!board[this.x + 1][this.y]) {
-        this.x + 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y}`);
-      }
-      if (this.x === 1 && !board[this.x + 2][this.y]) {
-        this.x + 2 <= 7 && possibleMoves.push(`${this.x + 2},${this.y}`);
-      }
-      if (
-        board[this.x + 1][this.y - 1] &&
-        board[this.x + 1][this.y - 1].side === "white"
-      ) {
-        this.x + 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y - 1}`);
-      }
-      if (
-        board[this.x + 1][this.y + 1] &&
-        board[this.x + 1][this.y + 1].side === "white"
-      ) {
-        this.x + 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y + 1}`);
+      if (this.x === 0) {
+        alert("wybierz bialego pionka");
       }
     }
-    if (this.side === "black" && this.x === 7) {
-      alert("wybierz czarnego pionka");
-      console.table(board);
-      console.log(this.x, this.y);
+
+    if (this.side === "black") {
+      if (this.x !== 7) {
+        if (!board[this.x + 1][this.y]) {
+          this.x + 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y}`);
+        }
+        if (this.x === 1 && !board[this.x + 2][this.y]) {
+          this.x + 2 <= 7 && possibleMoves.push(`${this.x + 2},${this.y}`);
+        }
+        if (
+          board[this.x + 1][this.y - 1] &&
+          board[this.x + 1][this.y - 1].side === "white"
+        ) {
+          this.x + 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y - 1}`);
+        }
+        if (
+          board[this.x + 1][this.y + 1] &&
+          board[this.x + 1][this.y + 1].side === "white"
+        ) {
+          this.x + 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y + 1}`);
+        }
+      }
+      if (this.x === 7) {
+        alert("wybierz czarnego pionka");
+      }
     }
+
     return possibleMoves;
   }
   promote() {}
