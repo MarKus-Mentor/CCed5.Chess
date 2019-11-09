f582007ddba08dd7be7c1b1c062669b899f97a5c
Joanna DS <j.dabrowska.slaby@gmail.com>
2019-11-09 23:12:20 +0100

    Setting the listerners to the last rows of pawns

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -1,23 +1,18 @@
 import Piece from "./piece";
 import board from "../board";
-
 class Pawn extends Piece {
   constructor(x, y, side) {
     super(x, y, side);
     this.name = "pawn";
-    this.display = `<i class="fas fa-chess-pawn ${side}"></i>`;
-    //this.display = `<img src= "../../img/pawn_3d_${side}.png" width=60 alt="pawn_${side}"/>`;
+    // this.display = `<i class="fas fa-chess-pawn ${side}"></i>`;
+    this.display = `<img src= "../../img/pawn_3d_${side}.png" width=60 alt="pawn_${side}"/>`;
   }
-
   findLegalMoves() {
     const possibleMoves = [];
-
-    //white
     if (this.side === "white" && this.x !== 0) {
       if (!board[this.x - 1][this.y]) {
         this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y}`);
       }
-
       if (this.x === 6 && !board[this.x - 2][this.y]) {
         this.x - 2 >= 0 && possibleMoves.push(`${this.x - 2},${this.y}`);
       }
@@ -34,7 +29,11 @@ class Pawn extends Piece {
         this.x - 1 >= 0 && possibleMoves.push(`${this.x - 1},${this.y + 1}`);
       }
     }
-    //black
+    if (this.side === "white" && this.x === 0) {
+      alert("wybierz bialego pionka");
+      console.table(board);
+      console.log(this.x, this.y);
+    }
     if (this.side === "black" && this.x !== 7) {
       if (!board[this.x + 1][this.y]) {
         this.x + 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y}`);
@@ -54,21 +53,15 @@ class Pawn extends Piece {
       ) {
         this.x + 1 <= 7 && possibleMoves.push(`${this.x + 1},${this.y + 1}`);
       }
-      let readyForPromotion =
-        (this.side === "black" && this.x === 7) ||
-        (this.side === "white" && this.x === 0);
-
-      if (readyForPromotion) {
-        alert("wybierz pionka");
-        console.log[this.x][this.y];
-      }
     }
-
+    if (this.side === "black" && this.x === 7) {
+      alert("wybierz czarnego pionka");
+      console.table(board);
+      console.log(this.x, this.y);
+    }
     return possibleMoves;
   }
-
   promote() {}
   enPassant() {}
 }
-
 export default Pawn;
