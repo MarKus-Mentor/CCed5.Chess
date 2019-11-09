f62570048641a44a40e5cc1511f7a922a53e8e42
Aneta <a.banas.brejt@gmail.com>
2019-11-09 21:58:06 +0100

    mój nieszczęsny push

--- src/board.js
+++ src/board.js
@@ -1,4 +1,5 @@
 import Pawn from './pieces/pawn';
+import Bishop from './pieces/bishop';
 
 const board = new Array(8);
 for (let i = 0; i < 8; i++) {
@@ -9,5 +10,14 @@ let pawn = new Pawn(6, 0, 'white');
 board[pawn.x][pawn.y] = pawn;
 pawn = new Pawn(6, 1, 'white');
 board[pawn.x][pawn.y] = pawn;
+
+let bishop = new Bishop(7,2, 'white');
+board[bishop.x][bishop.y] = bishop;
+bishop = new Bishop(7,5, 'white');	
+board[bishop.x][bishop.y] = bishop;
+bishop = new Bishop(0,2, 'black');
+board[bishop.x][bishop.y] = bishop;
+bishop = new Bishop(0,5, 'black');
+board[bishop.x][bishop.y] = bishop;
 
 export default board;
