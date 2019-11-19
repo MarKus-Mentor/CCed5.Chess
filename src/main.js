0c744b7e72ffb9de7273a247cd39929fcaf89be0
Ola <aleksandra@gruchala.eu>
2019-11-19 18:19:25 +0100

    Add timer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 import setup from './setup';
 
+let totalSeconds = localStorage.getItem('timer') || 0;
+let minutesLabel = document.getElementById("minutes");
+let secondsLabel = document.getElementById("seconds");
+setInterval(setTime, 1000);
+
+function pad(val) {
+  let valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  localStorage.setItem('timer', JSON.stringify(totalSeconds));
+}
+
 //checkWinner();
 
 //changePlayer(); to wszystko na później
